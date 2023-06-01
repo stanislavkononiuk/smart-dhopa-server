88c6b8538d47d2b883733f88654ed5a65aeface1
stanislavkononiuk <stanislavkononiuk@gmail.com>
2023-06-01 20:45:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect((err) => {
-	const orders = client.db('smartDhopa').collection('orders');
+	const orders = client.db('smartDhopa').collection('order');
 	console.log('Orders connection successfully');
 
 	// Update Order Status
